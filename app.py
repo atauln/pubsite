3010b066d145d05714247e4632abc35986154b2e
atauln <ataulnoor75@gmail.com>
2023-01-05 11:03:32 -0500

    scrapped some things

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return render_template('home.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/technologies')
+def technologies():
+    return render_template('/technologies')
